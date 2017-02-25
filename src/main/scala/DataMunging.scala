bcf35081d238479d87ec7f8738109edefc036a36
buddiex <mailbuddie@gmail.com>
2017-02-25 22:48:12 +0100

    resolved merge confilict

--- src/main/scala/DataMunging.scala
+++ src/main/scala/DataMunging.scala
@@ -8,18 +8,15 @@ object DataMunging {
     		getMinSpread()
     		getMinGoalDiff()
     }
-
     def getMinSpread() = {
     	val source = Source.fromFile("weather.dat")
         val min_spread = source.getLines.toList
-<<<<<<< HEAD
 	                           .drop(2) //the first line is a header and second line is a blank space so skip those lines here.
 		                       .map(_.split("\\s+")) // split on regex white space
 		                       .map(x => (x(1), x(2).stripSuffix("*").toDouble-x(3).stripSuffix("*").toDouble)) //clean the recourds as somelines have * and aslo conver to double
 		                       .reduceLeft((x,y) => if (x._2 < y._2) x else y) // get minimum spread
         println(s"1: the day with the smallest temparature spread is day ${min_spread._1} with a spread of ${min_spread._2} degrees")
         source.close()
-
     }
 
     def getMinGoalDiff() = {
@@ -31,14 +28,6 @@ object DataMunging {
 	                     .map(x =>(x(2), x(7).toInt - x(9).toInt)) 
 	                     .reduceLeft((x,y) => if (x._2 < y._2) x else y) 
         println(s"2: the team with the smallest difference for and against goal is ${team._1} with a difference of ${team._2} degrees")
-=======
-                         .drop(2) //the first line is a header and second line is a blank space so skip those lines here.
-	                 .map(_.split("\\s+")) // split on regex white space
-	                 .map(x => (x(1), x(2).stripSuffix("*").toDouble,x(3).stripSuffix("*").toDouble)) //clean the recourds as somelines have * and aslo conver to double
-	                 .map(x => (x._1, x._2, x._3, x._2-x._3)) //calculate the spread
-	                 .reduceLeft((x,y) => if (x._4 < y._4) x else y) // get minimum spread
-        println(s"the day with the smallest temparature spread is day ${min_spread._1} with a spread of ${min_spread._4} degrees ")
->>>>>>> 880f71f7fce683018dcfdb6f32ae065b0c2a9fae
         source.close()
 
     }
